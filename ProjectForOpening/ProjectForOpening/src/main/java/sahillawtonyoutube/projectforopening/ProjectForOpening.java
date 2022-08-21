0cb423cd62fd426cc5a02c53cacd58df711a8a93
sahillawtonprojects <111641859+sahillawtonprojects@users.noreply.github.com>
2022-08-21 11:12:08 +0200

    Add files via upload

---
+++ ProjectForOpening/ProjectForOpening/src/main/java/sahillawtonyoutube/projectforopening/ProjectForOpening.java
@@ -0,0 +1,8 @@
+package sahillawtonyoutube.projectforopening;
+
+public class ProjectForOpening {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
